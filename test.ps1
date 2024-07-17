1c35de29bd0434dd28be6354003f4c8c9b901e10
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-07-17 16:04:51 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,13 @@
 Write-Host "Hello"
-clear
+$nugetVersion = '6.10.1'
+$nugetUrl = "https://dist.nuget.org/win-x86-commandline/v$nugetVersion/nuget.exe"
+
+$nugetDir = "$env:SystemDrive\Tools\NuGet"
+
+(New-Object Net.WebClient).DownloadFile($nugetUrl, "$nugetDir\nuget.exe")
+
+(nuget).split("`n")[0]
+
+nuget sources add -name nuget.org -source https://api.nuget.org/v3/index.json
+
+Write-Host "NuGet updated" -ForegroundColor Green
