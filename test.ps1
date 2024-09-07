10680f2a63fc0ae50fa804b7be04fcb8ca51a049
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-09-07 12:59:00 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,17 @@
+# C:\Tools\curl\bin\curl.exe
+# $CurlArgument = '-u', 'xxx@gmail.com:yyyy',
+#                 '-X', 'POST',
+#                 'https://xxx.bitbucket.org/1.0/repositories/abcd/efg/pull-requests/2229/comments',
+#                 '--data', 'content=success'
+# $CURLEXE = 'C:\Program Files\Git\mingw64\bin\curl.exe'
+# & $CURLEXE @CurlArgument
+
+
+
+curl -fsS -o hdf5-1.8.13-win32-VS2010-shared.zip https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.13/bin/windows/hdf5-1.8.13-win32-VS2010-shared.zip
+
+
+
 Write-Host "Hello"
 $nugetVersion = '6.10.1'
 $nugetUrl = "https://dist.nuget.org/win-x86-commandline/v$nugetVersion/nuget.exe"
