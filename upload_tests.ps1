12f61a7d4aa0cf1545c529057b66acfcd5a52e0a
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-11-12 14:04:48 -0800

    Update upload_tests.ps1

--- upload_tests.ps1
+++ upload_tests.ps1
@@ -3,5 +3,5 @@
 $wc = New-Object 'System.Net.WebClient'
 
 $testResultFile = ".\TestResults.xml"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultFile))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultFile))
 
