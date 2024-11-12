acb432de217a1c7c6a4856e709dde81b3abc1fca
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2024-11-12 12:56:18 -0800

    Update upload_tests.ps1

--- upload_tests.ps1
+++ upload_tests.ps1
@@ -2,8 +2,6 @@
 
 $wc = New-Object 'System.Net.WebClient'
 
-foreach($project in $TestProjects)
-{
-    $testResultFile = ".\TestResults.xml"
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$project.TestResults.xml))
-}
+$testResultFile = ".\TestResults.xml"
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$project.TestResults.xml))
+
